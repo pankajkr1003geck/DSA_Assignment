bfddbe0e00c6b8bcb34525ad72764c8e61c07596
PANKAJ KUMAR <memrpankaj@gmail.com>
2026-02-20 01:31:36 +0530

    Delete CONDITIONAL STATEMENT directory

--- CONDITIONAL STATEMENT/Predict1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x = 10, y = 20;
-    if(x==y);
-        printf("\n%d %d",x,y);
-    return 0;
-}
--- CONDITIONAL STATEMENT/Predict2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x = 3, y = 5;
-    if(x==3)
-        printf("\n%d",x);
-    else;
-    printf("\n%d",y);
-    return 0;
-}
